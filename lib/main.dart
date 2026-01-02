50d1d2d1d921651b07037b0554557902db46e6db
recepzgrmh <recepzgrmh@gmail.com>
2026-01-02 18:41:30 +0300

    build: add dio and dotenv dependencies with initial config

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/app.dart';
 import 'app/di/di.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   configureDependencies();
   runApp(const App());
 }
