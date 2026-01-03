a4c90ddfaf9bfde44c559e09ddf9b65e58afc1d8
recepzgrmh <recepzgrmh@gmail.com>
2026-01-03 12:15:10 +0300

    feat(domain): add GenreEntity

---
+++ lib/features/home/domain/entities/genre_entity.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+
+/// Domain entity representing a movie genre
+class GenreEntity extends Equatable {
+  final int id;
+  final String name;
+
+  const GenreEntity({
+    required this.id,
+    required this.name,
+  });
+
+  @override
+  List<Object?> get props => [id, name];
+}
